283dd9ff01ac6d6624380aa406287b6c7fec04ac
Natalya.Kadysheva <Native1331>
2022-05-20 18:24:39 +0300

    Initial commit

---
+++ src/test/java/hhTests/HHTests.java
@@ -0,0 +1,67 @@
+package hhTests;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byLinkText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+@Tag ("hhTests")
+public class HHTests extends hhTests.TestBase {
+
+    @Test
+    @DisplayName("Open main page")
+    void openMainPage() {
+        $(".supernova-icon-services-dynamic").click();
+        $(".supernova-overlay__content").shouldHave(text("Сервисы для соискателей"));
+
+    };
+
+    @Test
+    @DisplayName("Chose town")
+    void moveToTheSection() {
+        $(".supernova-navi-item_area-switcher-button").click();
+        $(byLinkText("Санкт-Петербург")).click();
+        $(".supernova-navi").shouldHave(text("Санкт-Петербург"));
+
+
+    };
+
+    @Test
+    @DisplayName("Search for  vacancies")
+    void searchVacancies() {
+        $(".bloko-input_scale-large").setValue("Тестировщик");
+        $(".bloko-button_stretched").submit();
+        $(".vacancy-serp-content").shouldHave(text("Тестировщик"));
+    }
+
+    ;
+
+    @Test
+    @DisplayName("Choose a vacancy in my city")
+    void chooseArticle() {
+        zoom(0.2);
+        $(".supernova-navi-item_area-switcher-button").click();
+        $(byLinkText("Санкт-Петербург")).click();
+        $$("ul.multiple-column-list_narrow li").get(11).$("a").click();
+        $(".bloko-input").setValue("Тестировщик").pressEnter();
+        $(".vacancy-serp-content").shouldHave(text("Тестировщик"));
+    };
+
+    @Test
+    @DisplayName("Select news")
+    void selectNews() {
+        zoom(0.2);
+        $(byLinkText("Что такое «сильные стороны» и как их определить")).click();
+        $(".cms-header-content ").shouldHave(text("Что такое «сильные стороны» и как их определить"));
+    }
+}
+
+
+
+
+
+
+
+
