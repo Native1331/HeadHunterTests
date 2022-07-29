eef3a4c835794da43444515dd0d00cf2ea3b155d
Natalya Kadysheva <83497921+Native1331@users.noreply.github.com>
2022-07-29 23:33:32 +0300

    Update HHTests.java

--- src/test/java/tests/HHTests.java
+++ src/test/java/tests/HHTests.java
@@ -5,18 +5,18 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byLinkText;
 import static com.codeborne.selenide.Selenide.*;
 
-    @Tag("hhTests")
-    public class HHTests extends TestBase {
-        @Test
-        void openMainPage() {
-            $(".supernova-icon-services-dynamic").click();
-            $(".supernova-overlay__content").shouldHave(text("Сервисы для соискателей"));
-        }
+ @Tag("hhTests")
+ public class HHTests extends TestBase {
+ @Test
+ void openMainPage() {
+      $(".supernova-icon-services-dynamic").click();
+      $(".supernova-overlay__content").shouldHave(text("Сервисы для соискателей"));
+   }
 
-        @Test
-        void choseTown() {
-            $(".supernova-navi-item_area-switcher-button").click();
-            $(byLinkText("Санкт-Петербург")).click();
-            $(".supernova-navi").shouldHave(text("Санкт-Петербург"));
-        }
+ @Test
+ void choseTown() {
+      $(".supernova-navi-item_area-switcher-button").click();
+      $(byLinkText("Санкт-Петербург")).click();
+      $(".supernova-navi").shouldHave(text("Санкт-Петербург"));        }
     }
+}
